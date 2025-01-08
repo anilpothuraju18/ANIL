f2c920f9b4922f2b1aaac84d81aa1577795de9f2
anilpothuraju18 <anilpothuraju5@gmail.com>
2025-01-08 13:20:38 +0530

    new files is addeded

---
+++ Day 4/looping.js
@@ -0,0 +1,35 @@
+// for (let i=1; i<=5; i++){
+//     console.log("javascript");
+
+// } 
+
+// for(let i=0;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i+"is a even number");
+//     }else{
+//     console.log(i+"is a odd number");
+//     }
+// }
+
+//! MAJP to find the factorial of a number
+//! 5 = 120 ; 5*4*3*2*1
+function factorial(num) {
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
+  
+  console.log(factorial(5));
+
+  //! WAJP to find the factors of a number
+  //! 12 - 2, 4 , 3,6
+
+  // ! while loop
+  let i =1;
+  while(i <= 5)
+  {
+    console.log("Helli wake up and please interact with me");
+    i++;
+  }
