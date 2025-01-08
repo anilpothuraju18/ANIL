954ef519934b465186e22c7b937d1b83909bcdb6
anilpothuraju18 <anilpothuraju5@gmail.com>
2025-01-08 15:34:08 +0530

    newfile

--- Day 4/looping.js
+++ Day 4/looping.js
@@ -13,23 +13,29 @@
 
 //! MAJP to find the factorial of a number
 //! 5 = 120 ; 5*4*3*2*1
-function factorial(num) {
-    let result = 1;
-    for (let i = 1; i <= num; i++) {
-      result *= i;
-    }
-    return result;
-  }
+// function factorial(num) {
+//     let result = 1;
+//     for (let i = 1; i <= num; i++) {
+//       result *= i;
+//     }
+//     return result;
+//   }
   
-  console.log(factorial(5));
+//   console.log(factorial(5));
 
   //! WAJP to find the factors of a number
   //! 12 - 2, 4 , 3,6
 
   // ! while loop
-  let i =1;
-  while(i <= 5)
-  {
-    console.log("Helli wake up and please interact with me");
-    i++;
-  }
+  // let i =1;
+  // while(i <= 5)
+  // {
+  //   console.log("Helli wake up and please interact with me");
+  //   i++;
+  // }
+
+  // ! do-while loop
+  let i=1;
+  do{
+    console.log("Hello JavaScript");
+  }while(i>=5);
